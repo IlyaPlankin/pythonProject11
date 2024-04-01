04b017aebf7488817fbc226e5491d200aa8247d9
Student <liskotov@bk.ru>
2024-04-01 21:16:50 +0300

    создал table_structure.py

--- main.py
+++ main.py
@@ -1,49 +1,77 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Text, ForeignKey, CheckConstraint, Boolean, DateTime, insert
-from datetime import datetime
+from sqlalchemy import create_engine, insert
 
-metadata = MetaData()
+from table_structure import metadata, telsprav, ed
 # engine = create_engine("mysql+pymysql://alch:123456@10.10.101.193:3306/mydb")
 engine = create_engine("postgresql+psycopg2://postgres:123456@10.10.101.193:5432/db2")
-
-telsprav = Table('telsprav', metadata,
-                 Column('id', Integer(), primary_key=True),
-                 Column('name', String(50), nullable=False, index=True),
-                 Column('surname', String(50), nullable=True, index=True),
-                 Column('phone', String(20), nullable=False),
-                 Column('about', Text(), nullable=True),
-                 Column('address', String(200), nullable=True)
-                 )
-ed = Table('extradata', metadata,
-           Column('id', Integer(), primary_key=True),
-           Column('client_id', Integer, ForeignKey('telsprav.id')),
-           Column('age', Integer, nullable=False),
-           Column('heigest', Integer, nullable=True),
-           Column('country', String(50), nullable=False),
-           Column('City', String(50), nullable=False),
-           Column('businessman', Boolean, default=False),
-           Column('created', DateTime, default=datetime.now),
-           CheckConstraint('age > 18', name='age_check')
-           )
 
 connection = engine.connect()
 metadata.create_all(engine)
 
-
-ins = telsprav.insert().values(
-    name = 'Kirill',
-    surname = 'Morozov',
-    phone = '8999999999',
-    about ='prepod',
-    address = 'Sovetskiy pr'
-)
-print(ins)
-print(ins.compile())
-connection.execute(ins)
-
-
-
+# ins = telsprav.insert().values(
+#     name = 'Kirill',
+#     surname = 'Morozov',
+#     phone = '8999999999',
+#     about ='prepod',
+#     address = 'Sovetskiy pr'
+# )
+# block_insert=insert(telsprav)
+#
+# result = connection.execute(block_insert,[
+# {
+#     "name" : 'Ivan',
+#     "surname" : 'Ivanov',
+#     "phone" : '54555552',
+#     "about" : 'doktor',
+#     "address" : 'Nevskiy'
+#
+# },
+#
+# {
+#     "name" : 'Petr',
+#     "surname" : 'Petrov',
+#     "phone" : '5545562122',
+#     "about" : 'voditel',
+#     "address" : 'Sennaya sq'
+#
+# }
+# ])
+#
+# items = [
+# {
+#     "name" : 'Oleg',
+#     "surname" : 'Plankin',
+#     "phone" : '4545546514',
+#     "about" : 'strelok',
+#     "address" : 'Prospekt prosveshenia'
+# },
+# {
+#     "name" : 'Ilya',
+#     "surname" : 'Plankin',
+#     "phone" : '46456258588',
+#     "about" : 'povar',
+#     "address" : 'Kypchino'
+# }
+# ]
+#
+# result = connection.execute(block_insert,items)
+#
+#
+# print(ins)
+# print(ins.compile())
+# connection.execute(ins)
 
 
 connection.commit()
 
 print(engine)
+
+ed_block_insert=insert(ed)
+ed_items = [{
+        "client_id":3,
+        "age":1000,
+        "heigest":200,
+        "country":"Romnia",
+        "City":"Sigishoara",
+        "businessman":True}]
+result3=connection.execute(ed_block_insert, ed_items)
+connection.commit()
